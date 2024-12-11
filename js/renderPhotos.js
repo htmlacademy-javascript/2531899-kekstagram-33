a103d64b263fa60450918ed8ea8ea5bccdc69987
Lisa Chudesnova <lizzuka@gmail.com>
2024-12-11 14:38:22 +0100

    Вносит исправления

--- js/renderPhotos.js
+++ js/renderPhotos.js
@@ -1,38 +1,12 @@
-import { renderComments } from './renderComments.js';
+import { renderFullPhoto } from './renderFullPhoto.js';
 
 const miniatureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 const miniatureList = document.querySelector('.pictures');
 const miniatureListFragment = document.createDocumentFragment();
-const fullPictureWindow = document.querySelector('.big-picture');
-const fullPictureWindowClose = document.querySelector('.big-picture__cancel');
 
-fullPictureWindowClose.addEventListener('click', () => {
-  fullPictureWindow.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-});
-
-document.addEventListener('keydown', (evt) => {
-  if(evt.key === 'Escape') {
-    fullPictureWindow.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  }
-});
-
-function getFullPhoto(photo) {
-  fullPictureWindow.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-
-  fullPictureWindow.querySelector('.big-picture__img').querySelector('img').src = photo.url;
-  fullPictureWindow.querySelector('.likes-count').textContent = photo.likes;
-  fullPictureWindow.querySelector('.social__caption').textContent = photo.description;
-  fullPictureWindow.querySelector('.social__comment-total-count').textContent = photo.comments.length;
-
-  renderComments(photo.comments);
-}
-
-const createPhoto = function(photo) {
+const createPhoto = (photo) => {
   const pictureFrame = miniatureTemplate.cloneNode(true);
   pictureFrame.querySelector('.picture__img').src = photo.url;
   pictureFrame.querySelector('.picture__img').alt = photo.description;
@@ -40,22 +14,43 @@ const createPhoto = function(photo) {
   pictureFrame.querySelector('.picture__comments').textContent = photo.comments.length;
 
   pictureFrame.onclick = function () {
-    getFullPhoto(photo);
+    renderFullPhoto(photo);
   };
 
   return pictureFrame;
 };
 
-function createPhotoFeed(photos) {
+const createPhotoFeed = (photos) => {
   document.querySelectorAll('.picture').forEach((element) => element.remove());
 
   photos.forEach((element) => {
     const createElement = createPhoto(element);
     miniatureListFragment.appendChild(createElement);
-
   });
 
   miniatureList.append(miniatureListFragment);
-}
+};
 
-export {createPhotoFeed};
+let pictures = [];
+
+const onContainerClick = (evt) => {
+  const thumbnail = evt.target.closest('data-thumbnail-id');
+
+  if(!thumbnail) {
+    return;
+  }
+
+  evt.preventDefault();
+  const picture = pictures.find((item) => item.id === +thumbnail.dataset.thumbnailId
+  );
+
+  renderFullPhoto(picture);
+};
+
+const renderGallery = (currentPhotos) => {
+  pictures = currentPhotos;
+  createPhotoFeed(pictures);
+  miniatureList.addEventListener('click', onContainerClick);
+};
+
+export {createPhotoFeed, renderGallery};
--- js/validateForms.js
+++ js/validateForms.js
@@ -1,37 +1,22 @@
-import { sliderContainer, imagePreview } from './changeEffects';
+import { closeOverlay } from './changeEffects';
 import { sendData } from './serverData';
 
-const uploadPhotoInput = document.querySelector('.img-upload__input');
-const uploadPhotoOverlay = document.querySelector('.img-upload__overlay');
-const uploadPhotoOverlayClose = document.querySelector('.img-upload__cancel');
+const HASHTAGREGEXP = /^#[a-zа-яё0-9]{1,19}$/i;
+const MAX_HASHTAG_AMOUNT = 5;
+const MAX_DESCRIPTION_LENGTH = 140;
+
+const SubmitButtonText = {
+  IDLE: 'Опубликовать',
+  SENDING: 'Публикую...',
+};
 
 const uploadPhotoForm = document.querySelector('.img-upload__form');
 const hashtagField = document.querySelector('.text__hashtags');
 const descriptionField = document.querySelector('.text__description');
 const uploadSubmitButton = document.querySelector('.img-upload__submit');
 
-uploadPhotoInput.addEventListener('change', () =>{
-  uploadPhotoOverlay.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-
-  document.addEventListener('keydown', (evt) => {
-    if(evt.key === 'Escape' && !(document.activeElement === hashtagField || document.activeElement === descriptionField)) {
-      uploadPhotoOverlay.classList.add('hidden');
-      document.body.classList.remove('modal-open');
-      uploadPhotoInput.value = '';
-      sliderContainer.style.display = 'none';
-      imagePreview.style.filter = '';
-    }
-  });
-});
-
-uploadPhotoOverlayClose.addEventListener('click', () => {
-  uploadPhotoOverlay.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  uploadPhotoInput.value = '';
-  sliderContainer.style.display = 'none';
-  imagePreview.style.filter = '';
-});
+const templateSuccess = document.querySelector('#success').content;
+const templateError = document.querySelector('#error').content;
 
 const pristine = new Pristine(uploadPhotoForm, {
   classTo: 'img-upload__field-wrapper',
@@ -39,98 +24,91 @@ const pristine = new Pristine(uploadPhotoForm, {
   errorTextClass: 'img-upload__field-wrapper--error',
 });
 
-const hashtagRegExp = /^#[a-zа-яё0-9]{1,19}$/i;
-const MAX_HASHTAG_AMOUNT = 5;
-const MAX_DESCRIPTION_LENGTH = 140;
-
 let errorMessage;
 
-function checkValidHashtag(array) {
+const checkValidHashtag = (array) => {
   errorMessage = 'Невалидный хэштег';
-  return array.every((hashtag) => hashtagRegExp.test(hashtag));
-}
+  return array.every((hashtag) => HASHTAGREGEXP.test(hashtag));
+};
 
-function checkHashtagDuplicates(array) {
+const checkHashtagDuplicates = (array) => {
   errorMessage = 'Хэштеги не могут повторяться';
   return array.filter((hashtag, i) => array.indexOf(hashtag) !== i).length;
-}
+};
 
-function checkHashtagAmount(array) {
+const checkHashtagAmount = (array) => {
   errorMessage = `Максимум ${MAX_HASHTAG_AMOUNT} хэштегов`;
   return array.length <= MAX_HASHTAG_AMOUNT;
-}
+};
 
-function validateHashtags(value) {
-  const hashtags = value.toLowerCase().trim().split(' ');
+const validateHashtags = (value) => {
+  const hashtags = value.toLowerCase().trim().split(' ').filter((hashtag) => hashtag !== '');
   if (!hashtags[0] || (checkValidHashtag(hashtags) && !checkHashtagDuplicates(hashtags) && checkHashtagAmount(hashtags))) {
     uploadSubmitButton.removeAttribute('disabled');
     return true;
   }
   uploadSubmitButton.setAttribute('disabled', '');
   return false;
-}
+};
 
-function validateDescription(value) {
+const validateDescription = (value) => {
   if (value.length <= MAX_DESCRIPTION_LENGTH) {
     uploadSubmitButton.removeAttribute('disabled');
     return true;
   }
   uploadSubmitButton.setAttribute('disabled', '');
   return false;
-}
+};
 
 pristine.addValidator(hashtagField, validateHashtags, () => errorMessage);
 pristine.addValidator(descriptionField, validateDescription, `Максимальное количество символов - ${MAX_DESCRIPTION_LENGTH}`);
 
-const SubmitButtonText = {
-  IDLE: 'Опубликовать',
-  SENDING: 'Публикую...',
-};
-
-const templateSuccess = document.querySelector('#success').content;
-const templateError = document.querySelector('#error').content;
-
-function disabledButton(text) {
+const disabledButton = (text) => {
   uploadSubmitButton.disabled = true;
   uploadSubmitButton.textContent = text;
-}
+};
 
-function enableButton(text) {
+const enableButton = (text) => {
   uploadSubmitButton.disabled = false;
   uploadSubmitButton.textContent = text;
-}
+};
 
-function closeNotification(evt) {
+const buttonClickHandler = (evt) => {
+  evt.stopPropagation();
+  const existElement = document.querySelector('.success') || document.querySelector('.error');
+  const closeButton = existElement.querySelector('button');
+  if(evt.target === existElement || evt.target === closeButton) {
+    existElement.remove();
+    document.body.removeEventListener('click', buttonClickHandler);
+  }
+};
+
+const buttonKeydownHandler = (evt) => {
   evt.stopPropagation();
   const existElement = document.querySelector('.success') || document.querySelector('.error');
   const closeButton = existElement.querySelector('button');
   if(evt.target === existElement || evt.target === closeButton || evt.key === 'Escape') {
     existElement.remove();
-    document.body.removeEventListener('click', closeNotification);
-    document.body.removeEventListener('keydown', closeNotification);
+    document.body.removeEventListener('keydown', buttonKeydownHandler);
   }
-}
+};
 
-function appendNotification(template, trigger = null) {
+const appendNotification = (template, trigger = null) => {
   trigger?.();
   const notificationNode = template.cloneNode(true);
   document.body.append(notificationNode);
-  document.body.addEventListener('click', closeNotification);
-  document.body.addEventListener('keydown', closeNotification);
-}
+  document.body.addEventListener('click', buttonClickHandler);
+  document.body.addEventListener('keydown', buttonKeydownHandler);
+};
 
-async function sendFormData(formElement) {
+const sendFormData = async (formElement) => {
   if (pristine.validate() === true) {
     disabledButton(SubmitButtonText.SENDING);
 
     try {
       await sendData(new FormData(formElement));
       appendNotification(templateSuccess, () => {
-        uploadPhotoOverlay.classList.add('hidden');
-        document.body.classList.remove('modal-open');
-        uploadPhotoInput.value = '';
-        sliderContainer.style.display = 'none';
-        imagePreview.style.filter = '';
+        closeOverlay();
       });
     } catch (error) {
       appendNotification(templateError);
@@ -138,11 +116,13 @@ async function sendFormData(formElement) {
       enableButton(SubmitButtonText.IDLE);
     }
   }
-}
+};
 
-function formSubmitHandler(evt) {
+const formSubmitHandler = (evt) => {
   evt.preventDefault();
   sendFormData(evt.target);
-}
+};
 
 uploadPhotoForm.addEventListener('submit', formSubmitHandler);
+
+export { pristine, hashtagField, descriptionField };
